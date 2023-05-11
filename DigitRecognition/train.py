7a498dadb50d04c7d0330f6638307834a8e1a064
OleksandrDoskaliuk <sashadoskaliyk@gmail.com>
2023-05-11 18:36:53 +0300

    Update model creation

--- DigitRecognition/train.py
+++ DigitRecognition/train.py
@@ -1,61 +1,69 @@
 from numpy import ndarray
 import tensorflow as tf
+import tensorflow_datasets as tfds
 import argparse
 
 default_model_name = 'model.h5'
 
 
 def main():
-    model_name, testing = parse_cli_args()
+    model_name = parse_cli_args()
 
-    (picture_train, label_train), (picture_test, label_test) = load_mnist_data()
+    (ds_train, ds_test), ds_info = load_mnist_data()
 
-    if testing == 'y':
-        test_existing_model(model_name, picture_test, label_test)
-    else:
-        create_new_model_file(model_name, picture_train, label_train)
+    create_new_model_file(model_name, ds_train, ds_test, ds_info)
 
 
-def parse_cli_args() -> (str, str):
+def parse_cli_args() -> str:
     parser = argparse.ArgumentParser(
         description='This script is designed to create a digit recognition model and testing it using MNIST data')
     parser.add_argument('-m', '--model', type=str, default=default_model_name,
                         help='Model name. Note: provide full path to the model if it is not located in work dir')
-    parser.add_argument('-t', '--testing', type=str, choices=['y', 'n'], default='n',
-                        help='Test model flag. If `y` then test provided model. If `n` the new model will be created. Default - `n`')
 
     args = parser.parse_args()
 
-    return args.model, args.testing
+    return args.model
 
 
-def create_new_model_file(model_name: str, x_train, y_train):
-    picture_train_normalized = normalize_pictures_array(x_train)
+def create_new_model_file(model_name: str, ds_train, ds_test, ds_info):
+    ds_train = prepare_training_data(ds_train, ds_info)
+    ds_test = prepare_testing_data(ds_test)
 
     model = create_model()
 
-    compile_model(model)
-
-    train_model(model, picture_train_normalized, y_train)
+    train_model(model, ds_train, ds_test)
 
     save_model(model, model_name)
 
 
-def test_existing_model(model_name: str, x_test, y_test):
-    model = tf.keras.models.load_model(model_name)
+def load_mnist_data():
+    (ds_train, ds_test), ds_info = tfds.load(
+        'mnist',
+        split=['train', 'test'],
+        shuffle_files=True,
+        as_supervised=True,
+        with_info=True,
+    )
+    return (ds_train, ds_test), ds_info
 
-    loss, accuracy = model.evaluate(x_test, y_test)
 
-    print(f"Loss: {loss}")
-    print(f"Accuracy: {accuracy}")
+def prepare_training_data(ds_train, ds_info):
+    ds_train = ds_train.map(
+        normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_train = ds_train.cache()
+    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+    ds_train = ds_train.batch(128)
+    ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+    return ds_train
 
 
-def load_mnist_data() -> ((ndarray, ndarray), (ndarray, ndarray)):
-    mnist = tf.keras.datasets.mnist
-
-    (picture_train, label_train), (picture_test, label_test) = mnist.load_data()
-
-    return (picture_train, label_train), (picture_test, label_test)
+def prepare_testing_data(ds_test):
+    ds_test = ds_test.map(
+        normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_test = ds_test.batch(128)
+    ds_test = ds_test.cache()
+    ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+    return ds_test
 
 
 def normalize_pictures_array(pictures: ndarray) -> ndarray:
@@ -67,31 +75,39 @@ def create_model():
     units_number = 128
     output_digits_number = 10
 
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Flatten(input_shape=picture_shape))
-    model.add(tf.keras.layers.Dense(units_number, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(units_number, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(output_digits_number, activation=tf.nn.softmax))
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=picture_shape),
+        tf.keras.layers.Dense(units_number, activation='relu'),
+        tf.keras.layers.Dense(units_number, activation='relu'),
+        tf.keras.layers.Dense(output_digits_number)
+    ])
+
+    model.compile(
+        optimizer=tf.keras.optimizers.legacy.Adam(0.001),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
 
     return model
 
 
-def compile_model(model):
-    optimizer = 'adam'
-    loss = 'sparse_categorical_crossentropy'
-    metrics = ['accuracy']
+def train_model(model, ds_train, ds_test):
+    epochs = 10
 
-    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-
-
-def train_model(model, x_train, y_train):
-    epochs = 3
-
-    model.fit(x_train, y_train, epochs=epochs)
+    model.fit(
+        ds_train,
+        epochs=epochs,
+        validation_data=ds_test
+    )
 
 
 def save_model(model, model_name):
     model.save(model_name)
+
+
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
 
 
 if __name__ == '__main__':
